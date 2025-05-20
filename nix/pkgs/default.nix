e09d1f8cdc101672014b525e433b8ae58223d246
Sven Ziegler <43136984+Svenum@users.noreply.github.com>
2025-05-20 21:53:34 +0200

    move to iglu-flake (#7)
    * move to iglu-flake
    
    * fix missing iglu.flakecheck
    
    * remove unneded wrapper

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -1,5 +1,5 @@
 _: prev: {
-  iglu = {
+  iglu = prev.iglu // {
     iglu-controller = prev.callPackage ./iglu-controller { };
     iglu-controller-docker = prev.callPackage ./iglu-controller-docker { };
   };
