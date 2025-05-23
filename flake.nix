325846466f989f0537be6bc0f43f575d44bd2e42
Svenum <s.ziegler@holypenguin.net>
2025-05-23 16:54:06 +0200

    change org in other files

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     utils.url = "github:gytis-ivaskevicius/flake-utils-plus?ref=afcb15b845e74ac5e998358709b2b5fe42a948d1";
-    iglu-flake.url = "github:iglu-api/flake?ref=d9ca6b5d77b33ce20a0906b7c1491b792777bab5";
+    iglu-flake.url = "github:iglu-sh/flake?ref=d9ca6b5d77b33ce20a0906b7c1491b792777bab5";
   };
 
   # deadnix: skip
