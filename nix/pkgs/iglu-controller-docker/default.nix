e5b64c89544b35e19d2eb1a143dd1dc994de6114
Svenum <s.ziegler@holypenguin.net>
2025-06-02 07:26:59 +0200

    rename docker

--- nix/pkgs/iglu-controller-docker/default.nix
+++ nix/pkgs/iglu-controller-docker/default.nix
@@ -8,7 +8,7 @@ let
   archType = if (stdenv.hostPlatform.system == "x86_64-linux") then "amd64" else "arm64";
 in
 dockerTools.buildImage {
-  name = "iglu-controller-docker";
+  name = "iglu-controller";
   tag = "v${iglu.iglu-controller.version}-${archType}";
 
   copyToRoot = [
